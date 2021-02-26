5b31c635119b848a892035e0bcecefbd841a28e0
mandemt <70643363+mandemt@users.noreply.github.com>
2021-02-26 13:22:01 +0100

    public folder

--- index.js
+++ index.js
@@ -2,20 +2,12 @@ console.log('hallo world')
 
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 300;
 
 app.get('/', (req, res) => {
 	res.send('test 1234 test')
 });
 
-app.listen(port,() => {
-	console.log('de app luistert op poort port')
+app.listen(port, () => {
+	console.log('de app luistert op localhost:${port}')
 })
-
-app.use(function (req,res,next){
-	res.status(404).send('Kan niet vinden!!!!!!!')
-})
-app.listen(3000);
-
-
-module.exports = router;
