1765c9bb509ddae12e2d4c6f912fc5e114e54768
mandemt <70643363+mandemt@users.noreply.github.com>
2021-03-02 12:19:03 +0100

    backend huiswerk voor week 3 toegevoegd

--- index.js
+++ index.js
@@ -2,12 +2,44 @@ console.log('hallo world')
 
 const express = require('express');
 const app = express();
-const port = 300;
+const port = 3000;
 
-app.get('/', (req, res) => {
-	res.send('test 1234 test')
-});
+app.set('view engine', 'pug') // instellen voor view engine
 
-app.listen(port, () => {
-	console.log('de app luistert op localhost:${port}')
+app.get('/', (req,res) =>{
+	res.render('index', {title: 'Homgepage', message: 'Welkom bij de feature ' })//het respons bij het opvragen van de hoofdmap
 })
+
+
+
+app.get('/interesses', (req,res)=>{
+	res.render('interesses',{
+		title:'interesses',
+		voorNaam,
+		interesses})
+})
+//het respons bij het opvragen van de pagina waar je jouw interesses kan opgeven
+
+app.get('/resultaten', (req,res)=> {  // het respons bij het opvragen van de resultatenlijst
+	res.render('index', {title: 'Resultaten', message: 'Dit zijn mensen met de zelfde interesses' })
+})
+
+
+app.use(express.static('public')) // nu worden er static files opgevraagd vanuit een nadere map.
+
+
+app.listen(port, () =>{
+	console.log('de app lusitert op localhost:3000')
+})
+
+app.use(function(req,res,next){
+	res.status(404);
+	res.send('<h1>Gebruik een andere link!!!</h1>')
+})
+
+
+// nu de dynamic data
+
+const voorNaam = ["Hans", "Tom", "Piet"]
+const interesses = ["vissen", "gooien", "zagen"]
+
