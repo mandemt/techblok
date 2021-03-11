ad23e162e76fcd8ac82bbb44eb7a3cfc7cf27fc5
mandemt <70643363+mandemt@users.noreply.github.com>
2021-03-11 16:49:07 +0100

    backend

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.set('view engine', 'pug') // instellen voor view engine
 app.use(express.static('static')) // nu worden er static files opgevraagd vanuit een nadere map.
 
 
-const personen = [
+const gebruikers = [
 	{"id": "mandemt", "gebruikersnaam": "mandemt", "interesses": ["vissen", "hakken", "zingen"]},
 	{"id": "hans", "gebruikersnaam": "hans", "interesses": ["bakken", "braden", "hakken"]},
 	{"id": "piet", "gebruikersnaam": "piet", "interesses": ["bakken", "zingen", "vissen"]},
@@ -19,7 +19,31 @@ const personen = [
 	
 ];
 
+app.use(bodyParser.urlencoded({extended:false}))
 
+
+
+
+app.get('/toevoegen', (req, res)=>{
+	res.render('dynamic');
+	
+})
+
+
+
+app.post('/add', (req, res,)=>{
+	
+	const id = (req.body.name)
+	const gebruiker = {id: id, gebruikersnaam: req.body.naam, interesse: req.body.interesse};
+	const intrest = req.body.interesse;
+	const naam = req.body.naam;
+	console.log(naam);
+	console.log(req.body.interesse);
+	res.render('dynamic', {intrest, naam});
+	gebruikers.push(gebruiker)
+	
+	
+})
 app.get('/', (req,res) => {
 	res.render('index', {title: 'MagnetMatch'})
 })
@@ -32,8 +56,10 @@ app.get('/interesses.html', (req,res) => {
 	res.render('interesses', {title: 'Interesses'})
 })
 
-app.get('/resultaten.html', (req,res) => {
-	res.render('resultaten', {title: 'Interesses'})
+app.post('/resultaten.html', (req,res) => {
+	res.render('resultaten', {title: 'Interesses'});
+	const intrest = req.body.intrest;
+	console.log(intrest);
 })
 
 app.get('/persoon.html', (req,res) => {
@@ -65,8 +91,6 @@ app.use(function(req,res,next){
 
 
 // nu de dynamic data
-
-
 
 
 
