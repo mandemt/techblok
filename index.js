ca101569942227dcc267d35ca98cb3ff9ae460b0
mandemt <70643363+mandemt@users.noreply.github.com>
2021-03-11 23:19:49 +0100

    backend db

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 console.log('hallo world')
 
+
+
+
 const express = require('express'); // express wordt gebruikt
 const app = express(); 
 const port = 4000; // met localhost:3000 bezoek je de server in de browser
-
+const dotenv = require('dotenv').config();
+const {MongoClient} = require('mongodb');
 var bodyParser = require('body-parser') // het package bodyParser wordt gebruikt om het verwerken van data uit een request makkelijker te maken
 
 app.set('view engine', 'pug') // instellen voor view engine
@@ -11,46 +15,100 @@ app.set('view engine', 'pug') // instellen voor view engine
 app.use(express.static('static')) // nu worden er static files opgevraagd vanuit een nadere map.
 
 
+
+let db = null
+async function connectDB(){
+const uri = process.env.DB_URI
+const options = {useUnifiedTopology: true}
+const client = new MongoClient(uri,options);
+await client.connect(); // hierdoor worden geen andere taken uitgevoed totdat er verbonden is.
+db = await client.db(process.env.DB_NAME)
+}
+
+connectDB()
+	.then(() =>{
+	console.log('gelukt')
+	})
+	.catch(error =>{
+		console.log(error)
+	})
+
 const gebruikers = [
-	{"id": "mandemt", "gebruikersnaam": "mandemt", "interesses": ["vissen", "hakken", "zingen"]},
-	{"id": "hans", "gebruikersnaam": "hans", "interesses": ["bakken", "braden", "hakken"]},
-	{"id": "piet", "gebruikersnaam": "piet", "interesses": ["bakken", "zingen", "vissen"]},
-	{"id": "admin", "gebruikersnaam": "admin", "interesses": ["zingen"]},
+	{"id": "mandemt", "gebruikersnaam": "mandemt", "interesse": ["vissen", "hakken", "zingen"]},
+	{"id": "hans", "gebruikersnaam": "hans", "interesse": ["bakken", "braden", "hakken"]},
+	{"id": "piet", "gebruikersnaam": "piet", "interesse": ["bakken", "zingen", "vissen"]},
+	{"id": "admin", "gebruikersnaam": "admin", "interesse": ["zingen"]},
 	
 ];
 
 app.use(bodyParser.urlencoded({extended:false}))
 
-
-
-
-app.get('/toevoegen', (req, res)=>{
-	res.render('dynamic');
-	
-})
-
-
-
-app.post('/add', (req, res,)=>{
-	
-	const id = (req.body.name)
-	const gebruiker = {id: id, gebruikersnaam: req.body.naam, interesse: req.body.interesse};
-	const intrest = req.body.interesse;
-	const naam = req.body.naam;
-	console.log(naam);
-	console.log(req.body.interesse);
-	res.render('dynamic', {intrest, naam});
-	gebruikers.push(gebruiker)
-	
-	
-})
 app.get('/', (req,res) => {
 	res.render('index', {title: 'MagnetMatch'})
 })
 
-app.get('/inloggen.html', (req,res) => {
-	res.render('inloggen', {title: 'Inloggen'})
+
+app.get('/toevoegen.html', (req,res) => {
+	res.render('dynamic', {title: 'Inloggen'})
 })
+
+
+
+
+
+app.post('/toevoegen/add', (req, res,)=>{
+
+	const gebruiker = {id: req.body.naam, gebruikersnaam: req.body.naam, interesse: req.body.interesse};
+	
+	gebruikers.push(gebruiker);
+
+	
+	res.render('profiel', {gebruiker});
+
+	
+})
+
+
+app.get('/toevoegen/:gebruikersId', (req, res) => {
+	
+	
+
+	
+    const gebruiker = gebruikers.find( gebruiker => gebruiker.id == req.params.gebruikersId);
+	const interesse = req.body.interesse;
+	console.log('gebruikers')
+	console.log(anderen)
+    res.render('profiel', { gebruiker, gebruikers});
+	console.log(gebruiker)
+	console.log(anderen)
+		
+
+});
+
+
+
+
+
+
+app.listen(port, () =>{
+	console.log('de app lusitert op localhost:', port)
+})
+
+app.use(function(req,res,next){
+	res.status(404);
+	res.send('<h1>Gebruik een andere link!!!</h1>')
+})
+
+
+// nu de dynamic data
+
+
+
+
+
+
+
+
 
 app.get('/interesses.html', (req,res) => {
 	res.render('interesses', {title: 'Interesses'})
@@ -69,30 +127,5 @@ app.get('/persoon.html', (req,res) => {
 app.get('/opgeslagen.html', (req,res) => {
 	res.render('opgeslagen', {title: 'Interesses'})
 })
-
-
-
-
-
-
-
-
-
-
-
-app.listen(port, () =>{
-	console.log('de app lusitert op localhost:4000')
-})
-
-app.use(function(req,res,next){
-	res.status(404);
-	res.send('<h1>Gebruik een andere link!!!</h1>')
-})
-
-
-// nu de dynamic data
-
-
-
 
 
